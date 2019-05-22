276ffabe1cbc0588032f12190efcfa5343b7df1e
Vitaly <Baleskin1@gmail.com>
2019-05-22 14:57:44 +0300

    Configuration parser
    Вроде сделан

---
+++ Library/configuration_parser.py
@@ -0,0 +1,35 @@
+""""""
+class Block:
+    """"""
+    def __init__(self, block_name):
+        """"""
+        self.name = block_name
+        self.items = dict()
+
+    def add_item(self, item_name, item_value):
+        self.items[item_name]=item_value
+
+
+
+
+class ConfigurationParser:
+    """"""
+    def __init__(self, path):
+        """"""
+        self.path = path
+        self.blocks = dict()
+
+    def parse(self):
+        """"""
+        with open(self.path, 'r') as f:
+            block_name = ""
+            strs=f.readlines()
+            for line in strs:
+                if line[0]=='[':
+                    block_name = line[1:-2]
+                    self.blocks[block_name]=Block(block_name)
+                elif line!="":
+                    name, val = line.split('=')
+                    val = val[:-1] if val[-1]=='\n' else val
+                    self.blocks[block_name].add_item(name, val)
+        return self.blocks
---
+++ Tests/test_config.py
@@ -0,0 +1,19 @@
+""""""
+import unittest
+
+from Library.configuration_parser import ConfigurationParser
+
+class Test(unittest.TestCase):
+    """"""
+
+    def test_config(self):
+        cp = ConfigurationParser("..\\Scripts\\config.ini")
+        blocks = cp.parse()
+        for name, b in blocks.items():
+            print(name, ":\n")
+            for pname, pval in b.items.items():
+                print(pname," = ", pval,"\n")
+        assert True
+
+if __name__ == "__main__":
+    unittest.main()
